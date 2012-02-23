a47b35b06a7339b718dc08f5721bd7576b01bf43
Sam Aaron <samaaron@gmail.com>
2012-02-23 21:55:53 +0000

    add osc-clj as a dependency

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject snapshots "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[clojure "1.3.0"]])
+  :dependencies [[clojure "1.3.0"]
+                 [overtone/osc-clj "0.7.1"]])
